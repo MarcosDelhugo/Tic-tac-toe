78978965de640573c4afd45633865b074aa14671
MarcosDelhugo <marcos.delhugo@mi.unc.edu.ar>
2024-02-22 15:49:34 -0300

    Finalizando la logica del juego

--- logica.js
+++ logica.js
@@ -1,12 +1,64 @@
-var_state = true;
-function changeState(){
-    var_state=(var_state==true)?false:true
+var current_turn= true;
+var winner_found=false;
+const $buttons = document.querySelectorAll(".cell")
+
+function win_player() {
     
-    console.log(var_state)
-}
-const $buttons=document.getElementsByClassName("cell")
+    if(($buttons[0].value)&&($buttons[0].value===$buttons[1].value)
+        &&($buttons[1].value==$buttons[2].value)){//case first row
+        console.log("Gano " + $buttons[2].value)
+        winner_found=true;
+    }else if(($buttons[3].value)&&($buttons[3].value===$buttons[4].value)
+        &&($buttons[4].value==$buttons[5].value)){
+            console.log("Gano " + $buttons[3].value)//case second row
+            winner_found=true;
+    }else if(($buttons[6].value)&&($buttons[6].value===$buttons[7].value)
+        &&($buttons[7].value==$buttons[8].value)){
+            console.log("Gano " + $buttons[6].value)//case three row
+            winner_found=true;
+    }else if(($buttons[0].value)&&($buttons[0].value===$buttons[3].value)
+        &&($buttons[3].value==$buttons[6].value)){
+            console.log("Gano " + $buttons[6].value)//case frist column
+            winner_found=true;
+    }else if(($buttons[1].value)&&($buttons[1].value===$buttons[4].value)
+        &&($buttons[4].value==$buttons[7].value)){
+            console.log("Gano " + $buttons[4].value)//case second column
+            winner_found=true;
+    }else if(($buttons[2].value)&&($buttons[2].value===$buttons[5].value)
+        &&($buttons[5].value==$buttons[8].value)){
+            console.log("Gano " + $buttons[8].value)//case three column
+            winner_found=true;
+    }else if(($buttons[0].value)&&($buttons[0].value===$buttons[4].value)
+        &&($buttons[4].value==$buttons[8].value)){
+            console.log("Gano " + $buttons[8].value)//case frist diagonal
+            winner_found=true;
+    }
+    else if(($buttons[2].value)&&($buttons[2].value===$buttons[4].value)
+        &&($buttons[4].value==$buttons[6].value)){
+            console.log("Gano " + $buttons[6].value)//case second diagonal
+            winner_found=true;
+    }
 
-for (let i = 0; i < $buttons.length; i++) {
-    $buttons[i].onclick = changeState;
+    if(winner_found){
+        $buttons.forEach(function(){
+            for (let i = 0; i < $buttons.length; i++) {
+                $buttons[i].setAttribute("disabled","false")  
+            }
+        })
+    }
 }
 
+for (let i = 0; i < $buttons.length && !winner_found; i++) {
+    $buttons[i].onclick= function (e){
+        if(current_turn && !$buttons[i].value){
+            $buttons[i].setAttribute("value","X")
+            current_turn=false
+        }
+        else if (!$buttons[i].value){
+            $buttons[i].setAttribute("value","O")
+            current_turn=true
+        }
+        $buttons[i].setAttribute("disabled","false")
+        win_player();
+    }
+}
